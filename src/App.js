c3e6f827effa2d58186e4b6a524d4d526b9489b5
Kachanov2508 <kachanov2508@yandex.ru>
2021-01-26 21:51:15 +0300

    json-server

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Slider from "./Components/Slider/Slider";
 import "./styles.css";
 
 export default function App() {
+
+  const getData = () => {
+    fetch("http://localhost:3000/categories")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  getData();
+
   return (
     <div className="App">
       <Header />
